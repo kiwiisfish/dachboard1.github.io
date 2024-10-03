6ecb611c4636e8fc2e0134889f12be62ef2611bd
kiwiisfish <kiwiisfish@gmail.com>
2024-10-03 23:26:03 +0800

    v.1

---
+++ dashboard/order.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: '台灣積體電路股份有限公司',
+        productNumber: '2330',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: '台泥',
+        productNumber: '1100',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: '長榮',
+        productNumber: '2610',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
